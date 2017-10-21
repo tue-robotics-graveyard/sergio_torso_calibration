babc399ca3d7232f4b854a446b792545680f5c3f
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-10-21 11:07:48 +0200

    Update imu_angle0_to_angle0.m

--- matlab/imu_angle0_to_angle0.m
+++ matlab/imu_angle0_to_angle0.m
@@ -2,7 +2,7 @@ function out = imu_angle0_to_angle0(angle)
 
     run torso_measures_NX
     
-    offset_angle = an(RL_top-RL_bottom,[0;0;0],G-A);
+    offset_angle = an(RL_top-RL_bottom, [0;0;0], G-A);
     
     out = angle-offset_angle;
     
